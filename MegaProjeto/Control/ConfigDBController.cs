6c2c6f9ded6fdaa624211ce18e562436160610de
tchescofolda <ivan.f.e.folda@gmail.com>
2024-05-16 23:08:38 -0300

    testeAdicionar funcionalidade de exportação de relatórios em PDF

--- MegaProjeto/Control/ConfigDBController.cs
+++ MegaProjeto/Control/ConfigDBController.cs
@@ -4,11 +4,17 @@ using Newtonsoft.Json;
 using MegaProjeto.Model;
 using MegaProjeto.Viwer;
 using System.IO;
+using System.Windows.Shapes;
+using MegaProjeto.DAO;
 
 namespace MegaProjeto.Control
 {
-    public class ConfigDBController
+    public class ConfigDBController : MainController
+
     {
+        
+        MainController _controller = new MainController();
+        SocktDAO _conector = new SocktDAO();
         private ConfigDBForm view;
         //Seta o form ConfigDBForm
         public void SetView(ConfigDBForm view)
@@ -45,12 +51,11 @@ namespace MegaProjeto.Control
             string json = JsonConvert.SerializeObject(configDBModel);
 
             // Define o caminho completo do arquivo de configuração
-            string caminhoArquivo = @"C:\Users\Tchesco\source\repos\MegaProjeto\MegaProjeto\config.json";
 
             try
             {
                 // Escreve o JSON no arquivo
-                File.WriteAllText(caminhoArquivo, json);
+                File.WriteAllText(_controller.caminhoConfig, json);
 
                 MessageBox.Show("Configurações salvas com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
@@ -58,6 +63,33 @@ namespace MegaProjeto.Control
             {
                 MessageBox.Show("Ocorreu um erro ao salvar as configurações: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+        public void CarregarConfiguracoes()
+        {
+            
+
+            try
+            {
+                //var json = File.ReadAllText(_controller.caminhoConfig);
+                //var config = JsonConvert.DeserializeObject<ConfigDBModel>(json);
+                var config = _controller.RetonaConfBDJson();
+                // Preencher os TextBoxes com as informações do arquivo JSON
+
+                view.txServidor.Text = config.Servidor;
+                view.txPorta.Text = config.Porta.ToString();
+                view.txBanco.Text = config.Banco;
+                view.txUser.Text = config.Usuario;
+                view.txSenha.Text = config.Senha;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar configurações: " + ex.Message);
+            }
+        }
+
+        public void conecta()
+        {
+            _conector.conectar();
         }
     }
 }
